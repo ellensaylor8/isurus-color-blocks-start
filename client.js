9b93f3b10199af0e4ea8cc6ff4b1febe1c55e14f
Ellen Saylor <ellensaylor8@gmail.com>
2019-08-01 08:56:17 -0500

    initial commit

--- client.js
+++ client.js
@@ -2,5 +2,6 @@ $(document).ready(init);
 
 function init() {
     $('.js-container').append(`<div class="box green"></div>`)
-        .append(`<div class="box blue"></div>`);
+        .append(`<div class="box blue"></div>`).append(`<div class="box red"></div>`)
+        .append(`<div class="box yellow"></div>`);
 }
